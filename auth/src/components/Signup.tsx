f0893e5b7d4f9b2dffaec2450d14b4e50a218b69
Leonardo Santos <santos.leonardo2@hotmail.com>
2024-11-22 09:57:59 -0600

    Improving sign up form

--- auth/src/components/Signup.tsx
+++ auth/src/components/Signup.tsx
@@ -13,11 +13,23 @@ const Signup = () => {
         onSubmit={(e) => e.preventDefault()}
         style={{ display: 'flex', flexDirection: 'column' }}
       >
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" required />
+
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" />
+        <input type="email" id="email" required />
 
         <label htmlFor="password">Password</label>
-        <input type="password" id="password" />
+        <input type="password" id="password" required />
+
+        <div style={{ display: 'flex' }}>
+          <input
+            type="checkbox"
+            id="latestnews"
+            style={{ width: 'min-content' }}
+          />
+          <label htmlFor="latestnews">Subscribe to latest news</label>
+        </div>
 
         <button onClick={handleSubmit}>Log in</button>
         <p>
